5cf889b256d5bd8cd21e8b705f9b64c40c34e2b0
farhan <el3317.farhanmahmood@gmail.com>
2024-06-20 17:43:08 +0530

    changed the youtube channel link

--- src/components/constants/urlConstants.js
+++ src/components/constants/urlConstants.js
@@ -1,7 +1,7 @@
 export const LINKED_IN = "https://www.linkedin.com/in/10-farhan-mahmood/";
 export const GITHUB = "https://github.com/farhan2804";
 export const INSTAGRAM = "https://www.instagram.com/_farhan_2804/";
-export const YOUTUBE = "https://www.youtube.com/@animaginations7060";
+export const YOUTUBE = "https://www.youtube.com/@formulaC_Coder2804";
 export const EMAIL = "mailto:farhan.akp17el@gmail.com";
 export const TEXTTRACT = "https://farhan2804.github.io/textract/";
 export const TEXTTRACT_Source_Code = "https://github.com/farhan2804/textract";
