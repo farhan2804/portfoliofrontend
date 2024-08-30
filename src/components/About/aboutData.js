3b0a51a7f95bf6caf816f7e4df6e618c362225f2
farhan <el3317.farhanmahmood@gmail.com>
2024-08-30 16:52:03 +0530

    changed the about info

--- src/components/About/aboutData.js
+++ src/components/About/aboutData.js
@@ -1,10 +1,13 @@
-export const aboutText = `I'm currently navigating the exciting journey of B.Tech in Electronics and Communication at RVS College of Engineering and Technology, cruising through my 7th semester.
+export const aboutText = `
 
-My passion lies in crafting digital experiences, seamlessly blending technology with creativity to build innovative solutions. As a tech enthusiast, I thrive on exploring the ever-evolving landscape of web development and design.
+I recently graduated with a B.Tech in Electronics and Communication from RVS College of Engineering and Technology. My passion lies in crafting digital experiences, seamlessly blending technology with creativity to build innovative solutions. As a tech enthusiast, I thrive on exploring the ever-evolving landscape of web development and design.
 
-In my toolbox, I wield a versatile array of tools including React.js, JavaScript, Java, HTML, and CSS, among others. These technologies serve as my palette, allowing me to paint vibrant and intuitive user interfaces while ensuring smooth functionality under the hood.
+In my toolbox, I wield a versatile array of tools including React.js, JavaScript, Java, HTML, CSS, Node.js, and MongoDB, among others. These technologies serve as my palette, allowing me to paint vibrant and intuitive user interfaces while ensuring smooth functionality under the hood.
 
-Beyond mastering the frontend realm, I've delved deep into the world of data structures and algorithms (DSA), sharpening my problem-solving skills to tackle complex challenges efficiently. Moreover, my expertise extends to database management (DBMS), networking, and Node.js, providing me with a well-rounded skill set essential for building robust software solutions.
-Besides excelling in my college studies, I actively seek opportunities to apply my knowledge in electronics and communication. From circuit design to signal processing, I embrace every aspect of the field with enthusiasm and dedication.
+Moreover, my expertise extends to database management (DBMS), networking, and Node.js, providing me with a well-rounded skill set essential for building robust software solutions.
 
-Let's connect and explore the cool world where technology meets creativity!`;
+Having graduated, I'm now eager to apply my knowledge in both web development and electronics and communication. From circuit design to signal processing, I embrace every aspect of the field with enthusiasm and dedication.
+
+Let's connect and explore the cool world where technology meets creativity!
+
+`;
