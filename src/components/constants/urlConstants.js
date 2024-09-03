cc8f7173dd1e5d37dfcade601d70d1b9eaba4567
farhan <el3317.farhanmahmood@gmail.com>
2024-09-03 18:02:41 +0530

    changed the instagram link

--- src/components/constants/urlConstants.js
+++ src/components/constants/urlConstants.js
@@ -1,6 +1,6 @@
 export const LINKED_IN = "https://www.linkedin.com/in/10-farhan-mahmood/";
 export const GITHUB = "https://github.com/farhan2804";
-export const INSTAGRAM = "https://www.instagram.com/_farhan_2804/";
+export const INSTAGRAM = "https://www.instagram.com/formulac__coder28/";
 export const YOUTUBE = "https://www.youtube.com/@formulaC_Coder2804";
 export const EMAIL = "mailto:farhan.akp17el@gmail.com";
 export const TEXTTRACT = "https://farhan2804.github.io/textract/";
